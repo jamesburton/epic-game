e61081e398810d1d5020b65bc8c33027d69ccbf0
James Burton <james@code-consultants.co.uk>
2022-09-15 14:29:23 +0100

    Added networks and environment settings to allow deployment to actual networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,24 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomiclabs/hardhat-ethers";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const infuraKey = process.env.INFURA_KEY;
+const privateKey = process.env.PRIVATE_KEY;
+const networks = {};
+const networkNames = ['mainnet','kovan','rinkeby','goerli','sepolia','aurora','aurora-test'];
+if(infuraKey?.length) {
+  networkNames.forEach(name => networks[name] = {
+    url: `https://${name}.infura.io/v3/${infuraKey}`,
+    accounts: [privateKey]
+  });
+}
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks,
 };
 
 export default config;
